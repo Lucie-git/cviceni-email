f30ac2f27554038a0df71c060a1ad6fb0a3a40b9
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-03-29 16:26:56 +0100

    splněno

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const emailClosing = document.querySelector(".email__closing")
+
+const goodbye = (name = "Lucie") => {
+    emailClosing.textContent = "S pozdravem, " + name
+}
+
+const konecEmailu = goodbye()
 
