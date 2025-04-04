b1891900321d2bee78e413c68f9d3811264f5690
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-04-04 12:43:25 +0200

    Opravena fce goodbye

--- index.js
+++ index.js
@@ -2,17 +2,14 @@
 // 4. Zavolejte funkci na konci souboru `index.js` a ověřte, že v zobrazené stránce správně změní tělo e-mailu.
 // 5. Z předchozího cvičení nám zůstala funkce `goodbye`. Do funkce `fillBody` přidejte další parametr s názvem `name`. Tento parametr bude představovat jméno odesílatele. Funkce vyplní tělo e-mailu a do elementu `email__closing` vloží pozdrav, který vyrobí pomocí volání funkce `goodbye`.
 
-const emailClosing = document.querySelector('.email__closing');
-
-const goodbye = (name = 'Lucie') => {
+const goodbye = (name) => {
+  const emailClosing = document.querySelector('.email__closing');
   emailClosing.textContent = 'S pozdravem, ' + name;
 };
-
-const konecEmailu = goodbye();
+const konecEmailu = goodbye('Lucie Vadkerti');
 
 const fillSubject = (subject) => {
   const subjectElement = document.querySelector('.email__subject');
   subjectElement.textContent = subject;
 };
-
 fillSubject('Odpověď na inzerát');
