45125144c25f45f316077390e2f7c40743afa761
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-04-04 12:59:14 +0200

    doplněna fce fillBody

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// 3. Napište funkci `fillBody` s jedním parametrem `body`, která ze stránky vybere DOM element představující tělo e-mailu a nastaví jeho obsah dle hodnoty parametru.
-// 4. Zavolejte funkci na konci souboru `index.js` a ověřte, že v zobrazené stránce správně změní tělo e-mailu.
-// 5. Z předchozího cvičení nám zůstala funkce `goodbye`. Do funkce `fillBody` přidejte další parametr s názvem `name`. Tento parametr bude představovat jméno odesílatele. Funkce vyplní tělo e-mailu a do elementu `email__closing` vloží pozdrav, který vyrobí pomocí volání funkce `goodbye`.
-
 const goodbye = (name) => {
   const emailClosing = document.querySelector('.email__closing');
   emailClosing.textContent = 'S pozdravem, ' + name;
 };
-const konecEmailu = goodbye('Lucie Vadkerti');
+goodbye('Lucie Vadkerti');
 
 const fillSubject = (subject) => {
   const subjectElement = document.querySelector('.email__subject');
   subjectElement.textContent = subject;
 };
 fillSubject('Odpověď na inzerát');
+
+const fillBody = (body) => {
+  const bodyElement = document.querySelector('.email__body');
+  bodyElement.innerHTML = body;
+};
+fillBody(`<p>Ahoj světe!</p> <p>Konečně jsem pochpila princip funkcí!</p> <p>JUPÍÍÍ!!!</p>`);
